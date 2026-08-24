065cd9edc0db50b57046a18256cce465414f9991
Liuyang Wan <tsfdye@gmail.com>
2026-08-24 11:36:43 +0800

    Restore Discover large title (intentional)

--- app/(tabs)/(discover)/_layout.tsx
+++ app/(tabs)/(discover)/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from 'expo-router';
+import { useT } from '../../../lib/store';
 
 export default function DiscoverLayout() {
+  const t = useT();
+
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: '' }} />
+      <Stack.Screen name="index" options={{ title: t('appTitle'), headerLargeTitle: true }} />
     </Stack>
   );
 }
